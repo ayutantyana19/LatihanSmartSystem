7bd4e077728e3038401dcc90ad0df62b16902a7e
iwas108 <64204327+iwas108@users.noreply.github.com>
2021-12-06 22:06:51 +0800

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,5 @@
 #include <main.h>
 
-uint8_t jalan[4][3] = {
-  {11, 12, 13}, //timur
-  {8, 9, 10}, //barat
-  {5, 6, 7}, //selatan
-  {2, 3, 4} //utara
-};
-
-uint8_t durasiHatiHati = 1;
-uint8_t durasiJalan = 5;
-
 void setup()
 {
   for (auto &&arah : jalan){for (auto &&lampu : arah){pinMode(lampu, OUTPUT);}}
@@ -18,37 +8,4 @@ void setup()
 void loop()
 {
   for(uint8_t arah = 0; arah < 4; arah++){go(arah);}
-}
-
-void go(uint8_t arah)
-{
-  for(uint8_t index = 0; index < 4; index++)
-  {
-      digitalWrite(jalan[index][MERAH], HIGH);
-      digitalWrite(jalan[index][KUNING], LOW);
-      digitalWrite(jalan[index][HIJAU], LOW);
-  }
-
-  digitalWrite(jalan[arah][MERAH], LOW);
-  digitalWrite(jalan[arah][KUNING], HIGH);
-
-  uint16_t millisDulu = millis();
-  while(true)
-  {
-    uint16_t durasi = millis() - millisDulu;
-    if(durasi == (durasiHatiHati) * (uint16_t)1000)
-    {
-      digitalWrite(jalan[arah][KUNING], LOW);
-      digitalWrite(jalan[arah][HIJAU], HIGH);
-    }
-    else if(durasi == (durasiHatiHati+durasiJalan) * (uint16_t)1000)
-    {
-      digitalWrite(jalan[arah][HIJAU], LOW);
-      digitalWrite(jalan[arah][KUNING], HIGH);
-    }
-    else if(durasi == (durasiHatiHati+durasiJalan+durasiHatiHati) * (uint16_t)1000)
-    {
-      break;
-    }
-  }
 }
