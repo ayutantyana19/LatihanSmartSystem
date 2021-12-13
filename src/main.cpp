e2e1259f56abd784962db7b57d41d756b2640b79
iwas108 <64204327+iwas108@users.noreply.github.com>
2021-12-13 15:51:41 +0800

    added Temperature Alarm Multitasking

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,114 @@
 
 void setup()
 {
-  for (auto &&arah : jalan){for (auto &&lampu : arah){pinMode(lampu, OUTPUT);}}
+  Serial.begin(9600);
+
+  pinMode(A0, INPUT); //LM35
+  pinMode(MERAH, OUTPUT); //Merah
+  pinMode(KUNING, OUTPUT); //Kuning
+  pinMode(HIJAU, OUTPUT); //Hijau
+  pinMode(MULTITASKING_STATUS, OUTPUT); //Lampu tanda kode multitasking
 }
 
+uint16_t loopBefore = millis();
 void loop()
 {
-  for(uint8_t arah = 0; arah < 4; arah++){go(arah);}
+  uint16_t now = millis();
+  if( (now - loopBefore) > 50 )
+  {
+    digitalWrite(MULTITASKING_STATUS, !digitalRead(MULTITASKING_STATUS));
+    loopBefore = now;
+  }
+
+
+  cetakSuhu();
+  checkAlarm();
 }
+
+String checkAlarm()
+{
+  float celcius = getCelcius();
+
+  if(celcius >= 18 && celcius <= 30)
+  {
+    digitalWrite(MERAH, LOW);
+    digitalWrite(KUNING, LOW);
+    setAlarm(HIJAU, 100);
+    return "NORMAL";
+  }
+  else if(celcius > 30 && celcius <= 40)
+  {
+    digitalWrite(MERAH, LOW);
+    digitalWrite(HIJAU, LOW);
+    setAlarm(KUNING, 100);
+    return "AWAS";
+  }
+  else if(celcius > 40)
+  {
+    digitalWrite(HIJAU, LOW);
+    digitalWrite(KUNING, LOW);
+    setAlarm(MERAH, 100);
+    return "BAHAYA";
+  }
+  else
+  {
+    digitalWrite(MERAH, LOW);
+    digitalWrite(KUNING, LOW);
+    digitalWrite(HIJAU, LOW);
+    return "UNKNOWN";
+  }
+}
+
+uint16_t setAlarmBefore = millis();
+void setAlarm(uint8_t pin, uint16_t durasi)
+{
+  uint16_t now = millis();
+  if( (now - setAlarmBefore) > durasi )
+  {
+    digitalWrite(pin, !digitalRead(pin));
+    setAlarmBefore = now;
+  }
+}
+
+uint16_t cetakSuhuBefore = millis();
+void cetakSuhu()
+{
+  uint16_t now = millis();
+  if( (now - cetakSuhuBefore) > 1000 )
+  {
+    char buffer[64];
+    sprintf_P(buffer, PSTR("%sC - %sF - %sR - %sK (%s)"),
+      String(getCelcius()).c_str(), String(getFahren()).c_str(),
+      String(getReamur()).c_str(), String(getKelvin()).c_str(), checkAlarm().c_str());
+    Serial.println(buffer);
+    cetakSuhuBefore = now;
+  }
+}
+
+float getCelcius()
+{
+  int sinyalOutputTerkonversi = analogRead(A0);
+  float sinyalOutput = (sinyalOutputTerkonversi/1023.0) * 5000;
+  float celcius = (sinyalOutput / 10.0);
+
+  return celcius;
+}
+
+float getFahren()
+{
+  float fahren = (getCelcius() * 1.8) + 32;
+  return fahren;
+}
+
+float getReamur()
+{
+  float reamur = getCelcius() * 0.8;
+  return reamur;
+}
+
+float getKelvin()
+{
+  float kelvin = getCelcius() + 273.0;
+  return kelvin;
+}
+
--- src/main.h
+++ src/main.h
@@ -1,50 +1,14 @@
 #include <Arduino.h>
 
-#define MERAH 0
-#define KUNING 1
-#define HIJAU 2
+#define MERAH 13
+#define KUNING 12
+#define HIJAU 11
+#define MULTITASKING_STATUS 10
 
-uint8_t jalan[4][3] = {
-  {11, 12, 13}, //timur
-  {8, 9, 10}, //barat
-  {5, 6, 7}, //selatan
-  {2, 3, 4} //utara
-};
-
-uint8_t durasiHatiHati = 1;
-uint8_t durasiJalan = 5;
-
-void go(uint8_t arah);
-void go(uint8_t arah)
-{
-  for(uint8_t index = 0; index < 4; index++)
-  {
-      digitalWrite(jalan[index][MERAH], HIGH);
-      digitalWrite(jalan[index][KUNING], LOW);
-      digitalWrite(jalan[index][HIJAU], LOW);
-  }
-
-  digitalWrite(jalan[arah][MERAH], LOW);
-  digitalWrite(jalan[arah][KUNING], HIGH);
-
-  uint16_t millisDulu = millis();
-  while(true)
-  {
-    uint16_t durasi = millis() - millisDulu;
-    if(durasi == (durasiHatiHati) * (uint16_t)1000)
-    {
-      digitalWrite(jalan[arah][KUNING], LOW);
-      digitalWrite(jalan[arah][HIJAU], HIGH);
-    }
-    else if(durasi == (durasiHatiHati+durasiJalan) * (uint16_t)1000)
-    {
-      digitalWrite(jalan[arah][HIJAU], LOW);
-      digitalWrite(jalan[arah][KUNING], HIGH);
-    }
-    else if(durasi == (durasiHatiHati+durasiJalan+durasiHatiHati) * (uint16_t)1000)
-    {
-      break;
-    }
-  }
-}
-
+float getCelcius();
+float getFahren();
+float getReamur();
+float getKelvin();
+String checkAlarm();
+void cetakSuhu();
+void setAlarm(uint8_t pin, uint16_t durasi);
