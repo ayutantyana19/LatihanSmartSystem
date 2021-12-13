23bb725988019805841ccb38903b1d89df9952b4
iwas108 <64204327+iwas108@users.noreply.github.com>
2021-12-13 19:48:12 +0800

    LCD Test OK

--- src/main.cpp
+++ src/main.cpp
@@ -4,112 +4,31 @@ void setup()
 {
   Serial.begin(9600);
 
-  pinMode(A0, INPUT); //LM35
-  pinMode(MERAH, OUTPUT); //Merah
-  pinMode(KUNING, OUTPUT); //Kuning
-  pinMode(HIJAU, OUTPUT); //Hijau
-  pinMode(MULTITASKING_STATUS, OUTPUT); //Lampu tanda kode multitasking
+  while (lcd.begin(16, 2) != 1)
+  {
+    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal."));
+    delay(5000);
+  }
+
+  lcd.print(F("PCF8574 is OK..."));
+  delay(2000);
+
+  lcd.clear();
+
+  lcd.setCursor(0, 1);
+  lcd.print(F("Hello world!"));
+
+  lcd.setCursor(0, 2);
+  lcd.print(F("Random number:"));
 }
 
-uint16_t loopBefore = millis();
+
 void loop()
 {
-  uint16_t now = millis();
-  if( (now - loopBefore) > 50 )
-  {
-    digitalWrite(MULTITASKING_STATUS, !digitalRead(MULTITASKING_STATUS));
-    loopBefore = now;
-  }
+  /* print dynamic text */
+  lcd.setCursor(14, 2);
+  lcd.print(random(10, 1000));
+  lcd.write(LCD_SPACE_SYMBOL);
 
-
-  cetakSuhu();
-  checkAlarm();
+  delay(1000);
 }
-
-String checkAlarm()
-{
-  float celcius = getCelcius();
-
-  if(celcius >= 18 && celcius <= 30)
-  {
-    digitalWrite(MERAH, LOW);
-    digitalWrite(KUNING, LOW);
-    setAlarm(HIJAU, 100);
-    return "NORMAL";
-  }
-  else if(celcius > 30 && celcius <= 40)
-  {
-    digitalWrite(MERAH, LOW);
-    digitalWrite(HIJAU, LOW);
-    setAlarm(KUNING, 100);
-    return "AWAS";
-  }
-  else if(celcius > 40)
-  {
-    digitalWrite(HIJAU, LOW);
-    digitalWrite(KUNING, LOW);
-    setAlarm(MERAH, 100);
-    return "BAHAYA";
-  }
-  else
-  {
-    digitalWrite(MERAH, LOW);
-    digitalWrite(KUNING, LOW);
-    digitalWrite(HIJAU, LOW);
-    return "UNKNOWN";
-  }
-}
-
-uint16_t setAlarmBefore = millis();
-void setAlarm(uint8_t pin, uint16_t durasi)
-{
-  uint16_t now = millis();
-  if( (now - setAlarmBefore) > durasi )
-  {
-    digitalWrite(pin, !digitalRead(pin));
-    setAlarmBefore = now;
-  }
-}
-
-uint16_t cetakSuhuBefore = millis();
-void cetakSuhu()
-{
-  uint16_t now = millis();
-  if( (now - cetakSuhuBefore) > 1000 )
-  {
-    char buffer[64];
-    sprintf_P(buffer, PSTR("%sC - %sF - %sR - %sK (%s)"),
-      String(getCelcius()).c_str(), String(getFahren()).c_str(),
-      String(getReamur()).c_str(), String(getKelvin()).c_str(), checkAlarm().c_str());
-    Serial.println(buffer);
-    cetakSuhuBefore = now;
-  }
-}
-
-float getCelcius()
-{
-  int sinyalOutputTerkonversi = analogRead(A0);
-  float sinyalOutput = (sinyalOutputTerkonversi/1023.0) * 5000;
-  float celcius = (sinyalOutput / 10.0);
-
-  return celcius;
-}
-
-float getFahren()
-{
-  float fahren = (getCelcius() * 1.8) + 32;
-  return fahren;
-}
-
-float getReamur()
-{
-  float reamur = getCelcius() * 0.8;
-  return reamur;
-}
-
-float getKelvin()
-{
-  float kelvin = getCelcius() + 273.0;
-  return kelvin;
-}
-
--- src/main.h
+++ src/main.h
@@ -1,14 +1,6 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define MERAH 13
-#define KUNING 12
-#define HIJAU 11
-#define MULTITASKING_STATUS 10
-
-float getCelcius();
-float getFahren();
-float getReamur();
-float getKelvin();
-String checkAlarm();
-void cetakSuhu();
-void setAlarm(uint8_t pin, uint16_t durasi);
+#define LCD_SPACE_SYMBOL 0x20
+LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
